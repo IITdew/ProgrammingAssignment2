9acdfac5aed1edc25c5854b071048b9b5f803ddf
IITdew <jastrabik.pavel@gmail.com>
2025-07-28 10:36:23 +0200

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- NULL  # tu budeme uchovávať cache inverzie
+  
+  set <- function(y) {
+    x <<- y       # nastaví novú maticu
+    inv <<- NULL  # vymaže uloženú inverziu, lebo matica sa zmenila
+  }
+  
+  get <- function() x  # vráti aktuálnu maticu
+  
+  setinverse <- function(inverse) inv <<- inverse  # uloží inverziu do cache
+  getinverse <- function() inv                       # vráti cache inverziu
+  
+  # vrátime list funkcií, ktoré umožnia nastaviť a získať matica aj inverziu
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
